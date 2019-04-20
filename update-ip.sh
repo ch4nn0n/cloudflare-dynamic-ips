783ee3d90b4919edd25f2470d8c9a5f766608cc2
Joshua Channon <josh.channon@bitgamelabs.com>
2019-04-20 13:40:10 +0100

    Remove bash option pipefail
    This is for linux compatibility

--- update-ip.sh
+++ update-ip.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -o nounset
 set -o errexit
-set -o pipefail
 
 IP_REGEX="^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
 TZ=GMT
